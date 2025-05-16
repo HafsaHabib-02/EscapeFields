a59752233117b9f495c345daab63a829715a2a5a
HafsaHabib-02 <139891208+HafsaHabib-02@users.noreply.github.com>
2025-05-16 13:16:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -66,5 +66,7 @@ def thankyou():
     return render_template('thankyou.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # <-- this line is crucial
+    from dotenv import load_dotenv
+    load_dotenv()
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
